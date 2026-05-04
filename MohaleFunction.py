9444343da233e52b839b6607eb0cb43477e03804
Jai Mohale <74673775+Mohalian@users.noreply.github.com>
2026-05-03 22:11:45 -0400

    Update MohaleFunction.py
    Js moved a function into a class to make it fit better with the movement

--- MohaleFunction.py
+++ MohaleFunction.py
@@ -19,6 +19,72 @@ class Player:
         self.subplace = "frontofdoor"
         self.inventory = []
         
+
+    def updatePlayerPostion(self, choice, boardSize):
+        """
+    This function changes the player position and makes sure the user enters a
+    valid movement command
+    
+     Args:
+       choice: command entered by the user, has to have the specific command
+       move + a direction(string)
+       player_pos: Contains the x and y loc of the player(dict, keys either "x"
+       or "y")
+       boardSize: the size of the playable map(int)
+       
+    Side effects:
+        Changes player_pos
+
+    Returns: 
+        player_pos: the new position of the player (dict)
+    """
+    
+        keepLoop = True
+        xLoc = self.pos["x"]
+        yLoc = self.pos["y"]
+    
+        while keepLoop:
+        
+            if "move" not in choice:
+                print("Not a movement command")
+                return self.pos
+    
+            if "move north" in choice:
+        
+                yLoc = self.pos["y"]+1
+            
+    
+            elif "move south" in choice:
+        
+                yLoc = self.pos["y"]-1
+            
+    
+            elif "move west" in choice:
+        
+                xLoc = self.pos["x"]-1
+            
+    
+            elif "move east" in choice:
+        
+                xLoc = self.pos["x"]+1
+    
+            else:
+                choice = input("Movement command is move + (North, South, East, \
+                        West): ").lower()
+                continue  
+      
+            if xLoc >= boardSize or xLoc < 0 or yLoc >= boardSize or yLoc < 0:
+            
+                choice = input("You've been blocked by thick trees, \
+                           find another command: ").lower()
+                continue
+        
+            self.pos["x"] = xLoc
+            self.pos["y"] = yLoc
+            keepLoop = False
+    
+        return self.pos
+        
     def inventory_update(player, room, item_word, pick_drop):
         """
         Appends item objects into player's inventory list and removes from room's
@@ -80,72 +146,7 @@ class Player:
     # def updatePlayerPosition(self, choice, boardsize):
     
     # def inventoryUpdate(self, item_word, file, pick_drop):
-
-def updatePlayerPostion(choice, player_pos, boardSize):
-    """
-    This function changes the player position and makes sure the user enters a
-    valid movement command
-    
-     Args:
-       choice: command entered by the user, has to have the specific command
-       move + a direction(string)
-       player_pos: Contains the x and y loc of the player(dict, keys either "x"
-       or "y")
-       boardSize: the size of the playable map(int)
-       
-    Side effects:
-        Changes player_pos
-
-    Returns: 
-        player_pos: the new position of the player (dict)
-    """
-    
-    keepLoop = True
-    xLoc = player_pos["x"]
-    yLoc = player_pos["y"]
-    
-    while keepLoop:
-        
-        if "move" not in choice:
-            print("Not a movement command")
-            return player_pos
-    
-        if "move north" in choice:
-        
-            yLoc = player_pos["y"]+1
-            
-    
-        elif "move south" in choice:
-        
-            yLoc = player_pos["y"]-1
-            
-    
-        elif "move west" in choice:
-        
-            xLoc = player_pos["x"]-1
-            
-    
-        elif "move east" in choice:
-        
-            xLoc = player_pos["x"]+1
-    
-        else:
-            choice = input("Movement command is move + (North, South, East, \
-                        West): ").lower()
-            continue  
-      
-        if xLoc >= boardSize or xLoc < 0 or yLoc >= boardSize or yLoc < 0:
-            
-            choice = input("You've been blocked by thick trees, \
-                           find another command: ").lower()
-            continue
-        
-        player_pos["x"] = xLoc
-        player_pos["y"] = yLoc
-        keepLoop = False
-    
-    return player_pos
-        
+  
 class Item:
     
     def __init__(self, name, aliases, portable, interactions, description):
